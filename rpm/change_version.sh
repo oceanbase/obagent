8b4d99983a36394ccb5e690492d861d3e03e4779
zhj-luo <lzj0396@gmail.com>
2023-08-14 20:59:37 +0800

    Fix file header

--- rpm/change_version.sh
+++ rpm/change_version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (c) 2023 OceanBase
-# OCP Express is licensed under Mulan PSL v2.
+# OBAgent is licensed under Mulan PSL v2.
 # You can use this software according to the terms and conditions of the Mulan PSL v2.
 # You may obtain a copy of Mulan PSL v2 at:
 #          http://license.coscl.org.cn/MulanPSL2
